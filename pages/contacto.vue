9e393ea72778f8772c0b943533abf25bee914cb1
Alma29 <72773879+Alma29@users.noreply.github.com>
2022-11-15 14:37:17 -0600

    Se cambiaron colores / se agragaron textos/ se elimino el buzon y se puso link a WhatsApp

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -2,7 +2,7 @@
     <div>
         <Navbar />
         <section class="text-gray-600 body-font relative">
-  <div class="absolute inset-0 bg-gray-300">
+  <div class="absolute inset-0 bg-gray-200">
     <iframe 
     width="100%" 
     height="100%" 
@@ -17,19 +17,21 @@
 
   </div>
   <div class="container px-5 py-24 mx-auto flex">
-    <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Contáctanos</h2>
-      <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
+    <div class="lg:w-1/3 md:w-1/2 bg-deep-purple-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+      <h2 class="text-purple text-lg mb-1 font-bold title-font">Contáctanos</h2>
+      <p class="leading-relaxed mb-5 text-gray-700">Agenda tu cita por WhatsApp, estamos para atenderte.</p>
       <div class="relative mb-4">
-        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-      </div>
-      <div class="relative mb-4">
-        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-      </div>
-      <button class="text-white bg-purple border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-      <p class="text-xs text-gray-500 mt-3">Chicharrones blog</p>
+        <p class="mr-1 text-gray-700">WhatsApp:</p>
+          <a
+            href="https://api.whatsapp.com/send?phone=528134421227&text=%C2%A1Hola!%20Me%20gustar%C3%ADa%20agendar%20una%20cita.%20"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Our whatsapp"
+            title="Whatsapp"
+            class="transition-colors duration-300 text-purple hover:text-mint"
+            >81-3442-1227
+          </a>
+        </div>
     </div>
   </div>
 </section>
